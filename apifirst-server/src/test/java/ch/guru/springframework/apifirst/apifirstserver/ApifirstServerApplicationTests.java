ce7a6d4ffb3780a1c0733265def6dc2834130510
adm-boeckli <dboeckli@gmail.com>
2025-02-13 16:58:17 +0100

    setup project

---
+++ apifirst-server/src/test/java/ch/guru/springframework/apifirst/apifirstserver/ApifirstServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.guru.springframework.apifirst.apifirstserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifirstServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
