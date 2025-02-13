09590ab8252acbf2e4f6cacb5975835a154cc51f
adm-boeckli <dboeckli@gmail.com>
2025-02-13 15:53:17 +0100

    initial commit

---
+++ src/test/java/ch/guru/springframework/apifirst/apifirstsbserver/ApifirstSbServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.guru.springframework.apifirst.apifirstsbserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifirstSbServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
