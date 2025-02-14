5d9ed5a1410c5b5c4d5446d8869be76c664046a1
adm-boeckli <dboeckli@gmail.com>
2025-02-14 09:58:21 +0100

    setup jacoco for multi module project

--- apifirst-server/src/test/java/ch/guru/springframework/apifirst/apifirstserver/ApifirstServerApplicationTests.java
+++ apifirst-server/src/test/java/ch/guru/springframework/apifirst/apifirstserver/ApifirstServerApplicationTests.java
@@ -1,13 +1,24 @@
 package ch.guru.springframework.apifirst.apifirstserver;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@Slf4j
 class ApifirstServerApplicationTests {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Test
     void contextLoads() {
+        assertNotNull(applicationContext, "Application context should not be null");
+        log.info("Testing Spring 6 Application {}", applicationContext.getApplicationName());
     }
 
 }
