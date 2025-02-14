152649f820b1588e2ad8f00f5f21e6058289fbe1
adm-boeckli <dboeckli@gmail.com>
2025-02-14 16:09:24 +0100

    add validator

---
+++ apifirst-server/src/test/java/ch/guru/springframework/apifirst/apifirstserver/config/OpenApiValidationConfig.java
@@ -0,0 +1,37 @@
+package ch.guru.springframework.apifirst.apifirstserver.config;
+
+import com.atlassian.oai.validator.OpenApiInteractionValidator;
+import com.atlassian.oai.validator.springmvc.OpenApiValidationFilter;
+import com.atlassian.oai.validator.springmvc.OpenApiValidationInterceptor;
+import jakarta.servlet.Filter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class OpenApiValidationConfig {
+    
+    @Bean
+    public Filter validationFilter() {
+        return new OpenApiValidationFilter(
+            false, // disable request validation
+            true  // enable response validation
+        );
+    }
+
+    @Bean
+    public WebMvcConfigurer openAPIValidationInterceptor() {
+        OpenApiInteractionValidator validator = OpenApiInteractionValidator.createForSpecificationUrl("https://dboeckli.redocly.app/_spec/openapi/openapi.yaml")
+            .build();
+        OpenApiValidationInterceptor interceptor = new OpenApiValidationInterceptor(validator);
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                registry.addInterceptor(interceptor);
+            }
+        };
+    }
+    
+}
